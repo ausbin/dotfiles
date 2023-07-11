90c7fa955aaa68be91bbb64d16e4f07376571883
Austin Adams <git@austinjadams.com>
2023-07-11 12:37:07 -0400

    install.sh: Don't install README.md as ~/.README.md

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 me=$(readlink -f "${BASH_SOURCE[0]}")
+readme=$(readlink -f README.md)
 
 for f in *; do
     fullpath=$(readlink -f "$f")
 
-    if [[ $fullpath = $me ]]; then
+    if [[ $fullpath = $me || $fullpath = $readme ]]; then
         continue
     elif [[ -e ~/."$f" ]]; then
-        printf 'skipping installing existing file .%s...\n' "$f"
+        printf 'skipping installing existing file/directory .%s...\n' "$f"
         continue
     fi
 
